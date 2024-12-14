29f7a77ecd3621bb1434f278bed343f36fe543b3
Prajwol Kharel <xyzkharelprajwol@gmail.com>
2024-12-14 14:48:08 +0545

    Test for Command

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,16 @@
+"""
+Test custom Django management commands.
+"""
+
+from unittest.mock import patch
+from psycopg2 import OperationalError as Psycopg2Error
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+class CommandTests(SimpleTestCase):
+    """Test Commands."""
+
+    def test_wait_for_db_ready(self):
+        
+
