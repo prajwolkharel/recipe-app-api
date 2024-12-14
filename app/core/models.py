74eb9e4537aa7dea79e4aa73fdaf513dfdfa99cc
Prajwol Kharel <xyzkharelprajwol@gmail.com>
2024-12-15 00:07:48 +0545

    Fixed Linting

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,3 @@
-from django.db import models
+from django.db import models  # noqas
 
 # Create your models here.
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -8,13 +8,14 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test Commands."""
 
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database is ready."""
-        patched_check.return_value=True
+        patched_check.return_value = True
 
         call_command('wait_for_db')
 
@@ -23,13 +24,11 @@ class CommandTests(SimpleTestCase):
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting operational error."""
-        patched_check.side_effect = [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
 
         patched_check.assert_called_with(databases=['default'])
-
-
-
