1cc15e9531e15eb20489de330641d607c7f51723
Prajwol Kharel <xyzkharelprajwol@gmail.com>
2024-12-15 21:02:56 +0545

    Configured urls for documentation

--- app/app/settings.py
+++ app/app/settings.py
@@ -133,5 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
-    'DEFAULT-SCHEMA-CLASS': 'drf_spectacular.openai.AutoSchema'
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
