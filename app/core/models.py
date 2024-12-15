a32f725c9e7a16e472be61b48e7e40babeea175a
Prajwol Kharel <xyzkharelprajwol@gmail.com>
2024-12-15 11:48:34 +0545

    Fixed Linting

--- app/core/models.py
+++ app/core/models.py
@@ -12,7 +12,7 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
     """Manager for users."""
 
-    def create_user(seld, email, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
