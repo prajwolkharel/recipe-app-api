eed3514b13ade1dacd61afc20b4ab1856329a624
Prajwol Kharel <xyzkharelprajwol@gmail.com>
2024-12-16 13:13:01 +0545

    Implemented manage user api

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -103,10 +103,11 @@ class PublicUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_retrieve_user_unauthorized(self):
-        """Test authenticationis required for users."""
+        """Test authentication is required for users."""
         res = self.client.get(ME_URL)
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
 
 
 class PrivateUserApiTests(TestCase):
